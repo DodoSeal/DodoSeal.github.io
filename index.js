aba7ad2c0f1dc92737d9f2b190270fe497ddc3eb
DodoSeal <maxhmcd08@gmail.com>
2024-03-25 17:40:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// Background Image Picker
+/* let bgList = [
+    "BirdFoggyForest.jpeg",
+    "FogForest.jpeg",
+    "FogLandscape.jpeg",
+    "FogMountain.jpeg",
+    "PersonOnDockInRain.jpeg",
+    "RainyRoad.jpeg",
+    "RiverForest.jpeg",
+    "TropicalForest.jpeg"
+];
+
+let randomBgName = bgList[Math.floor(Math.random() * bgList.length)];
+
+document.body.style.backgroundImage = `url("./assets/background/${randomBgName}")`; */
+document.body.style.backgroundImage = 'url("./assets/background/RainyRoad.jpeg")';
+document.body.style.backgroundRepeat = `no-repeat`;
+document.body.style.backgroundSize = `cover`;
+
+// Glass Rotation
+let rotationDeg = 0;
+let glass = document.getElementById('glass');
+
+glass.addEventListener('click', (e) => rotateGlass(45));
+
+function rotateGlass(degrees) {
+    rotationDeg += degrees;
+    glass.style.transform = `rotate(${rotationDeg}deg)`;
+};
+
+// Socials
+let discord = document.getElementById('discord');
+let twitter = document.getElementById('twitter');
+let github = document.getElementById('github');
+let email = document.getElementById('email');
+
+discord.addEventListener('click', () => rotateGlass(-45));
+twitter.addEventListener('click', () => socialsRedirect("https://x.com/@WestJordan08"));
+github.addEventListener('click', () => socialsRedirect("https://github.com/DodoSeal"));
+email.addEventListener('click', () => socialsRedirect("mailto:admin@dodoseal.com"));
+
+function socialsRedirect(url) {
+    window.open(url, "about:");
+    rotateGlass(-45);
+};
