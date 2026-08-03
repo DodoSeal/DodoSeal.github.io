067d3134740a6f3b7a1ae89838fc5280388945d9
DodoSeal <maxhmcd08@gmail.com>
2026-08-02 19:26:11 -0600

    Feat: Design ProjectCard.svelte with image, title, description, and tags

---
+++ src/lib/assets/ProjectCard-Tag.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    interface TagInfo {
+        tag: string
+    };
+
+    const data: TagInfo = $props();
+</script>
+
+<div class="w-fit h-7 px-3 content-center bg-backdrop rounded-md hover:border-gray-600 border-gray-700 border-2">
+    <p class="text-text-light text-sm">{data.tag}</p>
+</div>
--- src/lib/assets/ProjectCard.svelte
+++ src/lib/assets/ProjectCard.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
+	import ProjectCardTag from "./ProjectCard-Tag.svelte";
+
     interface ProjectInfo {
         title: string,
         description: string,
         imageSrc: string,
-        tags: string[]
+        projectUrl: string,
+        tags: string[],
     };
 
     const data: ProjectInfo = $props();
 </script>
 
-<div class="max-w-5x h-100 bg-gray-800 rounded-2xl">
+<a href={data.projectUrl} target="_blank" class="max-w-5xl h-120 scrollbar-none overflow-hidden bg-gray-800 rounded-2xl group/card transition-all duration-200 hover:border-gray-600 border-gray-700 border-2 cursor-pointer">
+    <div class="w-full aspect-video bg-white scrollbar-none center-content overflow-hidden">
+        <img class="scale-105 group-hover/card:scale-[107%] transition-transform duration-200" src="{data.imageSrc}" alt="{data.title}-image">
+    </div>
 
-</div>
+    <div class="w-full h-fit max-h-[30%] px-6 py-4">
+        <h1 class="text-text-light text-lg">{data.title}</h1>
+        <p class="text-text-dark text-md mt-1">{data.description}</p>
+    </div>
+
+    <div class="w-full max-h-[15%] px-5 flex flex-wrap [&_div]:m-1 items-center">
+        {#each data.tags as tag}
+            <ProjectCardTag tag={tag} />
+        {/each}
+    </div>
+</a>
