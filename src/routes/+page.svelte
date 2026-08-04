489fc8b7efbe3482179998f3d21ed9f6713d3d71
DodoSeal <maxhmcd08@gmail.com>
2026-08-03 20:22:17 -0600

    Feat: Add Favorite Projects section to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,21 @@
 	import Footer from "$lib/assets/Footer.svelte";
 	import Navbar from "$lib/assets/Navbar.svelte";
 	import PageTitle from "$lib/assets/PageTitle.svelte";
+	import ProjectCard from "$lib/assets/ProjectCard.svelte";
+	import CardSection from "$lib/assets/CardSection.svelte";
 	import aboutMe from '$lib/assets/about-me.txt?raw';
 </script>
 
 <div class="main-wrapper">
     <Navbar />
     <PageTitle title="Hey, I am Max! 👋" description={aboutMe}/>
-    <div class="main-content center-content text-text-light">
-        <!-- Stuff about me goes here btw -->
+    <!-- <div class="main-content text-text-light">
         <p class="text-xl md:text-5xl self-start text-yellow-400 text-center">This website is under construction! Feel free to explore my work at <a class="text-blue-500 underline underline-offset-2" target="_blank" href="https://github.com/dodoseal">https://github.com/dodoseal</a></p>
-    </div>
+    </div> -->
+
+    <CardSection sectionTitle="Favorite Projects">
+        <ProjectCard title="Airship CLI" description="Interact with the Airship.gg API from your Terminal!" imageSrc="/ProjectImages/AirshipCLI.png" projectUrl="https://github.com/dodoseal/airship-cli" tags={[ "TypeScript", "Node.js" ]} />
+        <ProjectCard title="Drift" description="A Skyblock inspired farming game." imageSrc="/ProjectImages/Drift.png" projectUrl="https://airship.gg/g/384d911c-a5e8-4fda-be18-f715e8b2f116" tags={[ "TypeScript", "Unity Engine", "Airship" ]} />
+    </CardSection>
     <Footer />
 </div>
