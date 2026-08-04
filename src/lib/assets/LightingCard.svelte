003dccc27e65bbd7a377443e0960909646bf0cd5
DodoSeal <maxhmcd08@gmail.com>
2026-08-03 20:32:27 -0600

    Fix: Lazy image loading

--- src/lib/assets/LightingCard.svelte
+++ src/lib/assets/LightingCard.svelte
@@ -13,7 +13,7 @@
 
 <div class="max-w-5xl h-120 scrollbar-none overflow-hidden bg-gray-800 rounded-2xl group/card transition-all duration-200 hover:border-gray-600 border-gray-700 border-2">
     <div class="w-full aspect-video bg-white scrollbar-none center-content overflow-hidden">
-        <img class="scale-105 group-hover/card:scale-[107%] transition-transform duration-200" src="{data.imageSrc}" alt="{data.title}-image">
+        <img class="scale-105 group-hover/card:scale-[107%] transition-transform duration-200" loading="lazy" src="{data.imageSrc}" alt="{data.title}-image">
     </div>
 
     <div class="w-full h-fit max-h-[30%] px-6 py-4">
