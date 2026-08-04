c30010c214d8a82fea532878b3d822b23c7bc74e
DodoSeal <maxhmcd08@gmail.com>
2026-08-03 19:37:58 -0600

    Feat: Add more project images Feat: Add project sections component with section title

---
+++ src/lib/assets/ProjectSection.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    const { children, sectionTitle } = $props();
+</script>
+
+<!-- Section Title -->
+<div class="flex center-content w-full h-12">
+    <h1 class="text-center text-text-light text-3xl z-10 bg-backdrop px-6">{sectionTitle}</h1>
+    <div class="absolute w-[80%] h-0.5 bg-text-light"></div>
+</div>
+
+<!-- Container -->
+<div class="gap-6 grid grid-cols-1 md:grid-cols-2 pt-6 mb-6">
+    {@render children?.()}
+</div>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -3,6 +3,7 @@
 	import Navbar from "$lib/assets/Navbar.svelte";
 	import PageTitle from "$lib/assets/PageTitle.svelte";
 	import ProjectCard from "$lib/assets/ProjectCard.svelte";
+	import ProjectSection from "$lib/assets/ProjectSection.svelte";
 	import { onMount } from "svelte";
     let sitePath: string = $state("");
 
@@ -14,20 +15,27 @@
 <div class="main-wrapper">
     <Navbar />
     <PageTitle title="Projects" description="A display of my open-source work and hobbies :D"/>
-    <div class="gap-6 grid grid-cols-1 md:grid-cols-2 pt-6">
+
+    <ProjectSection sectionTitle="Hobby Projects">
         <ProjectCard title="DodoSeal.com" description="My personal website. You're here right now!" imageSrc="/ProjectImages/PersonalWebsite.png" projectUrl="https://github.com/dodoseal/dodoseal.github.io" tags={[ "Svelte", "TypeScript", "TailWind CSS" ]} />
-        <ProjectCard title="Airship CLI" description="Interact with the Airship.gg API from your Terminal!" imageSrc="/ProjectImages/AirshipCLI.gif" projectUrl="https://github.com/DodoSeal/airship-cli" tags={[ "TypeScript", "Node.js" ]} />
-        <ProjectCard title="Easy Games Notifier" description="A Discord bot that polls the Airship Games API to discover new content from Easy.gg." imageSrc="/ProjectImages/EasyNotifier.png" projectUrl="https://github.com/DodoSeal/EZGG-Notification-Bot" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
-        <ProjectCard title="Airship Bundle Downloader" description="Download Airship.gg Game Bundles using a CLI." imageSrc="" projectUrl="https://github.com/DodoSeal/Airship-Bundle-Downloader" tags={[ "TypeScript", "Node.js" ]} />
         <ProjectCard title="C Playground" description="All of my open-source C projects." imageSrc="/ProjectImages/C_Playground.png" projectUrl="https://github.com/DodoSeal/C-Playground" tags={[ "C" ]} />
-        <ProjectCard title="LPlot" description="A simple tool to design and manage Lighting Plots for any sized productions." imageSrc="" projectUrl="https://github.com/DodoSeal/LPlot" tags={[ "TypeScript", "Svelte", "Node.js", "Firebase", "Express.js" ]} />
-        <ProjectCard title="Walk to Win" description="A simple game where walking earns points. Made with Airship.gg" imageSrc="" projectUrl="https://github.com/DodoSeal/airship-walking" tags={[ "TypeScript", "Unity Engine", "Airship" ]} />
-        <ProjectCard title="Airship API Discord Bot" description="A powerful Discord Bot to fetch public information from Airship.gg." imageSrc="" projectUrl="https://github.com/DodoSeal/AirshipAPI-DiscordBot" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
-        <ProjectCard title="Islands Economy Discord Bot" description="Fetch Roblox Islands economy information, such as pricing guides and item rarities." imageSrc="" projectUrl="https://islandsbot.dodoseal.com" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
-        <ProjectCard title="Airship Docs Discord Bot" description="Lookup resources from the Airship documentation." imageSrc="" projectUrl="" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
+        <ProjectCard title="LPlot" description="A simple tool to design and manage Lighting Plots for any sized productions." imageSrc="/ProjectImages/LPlot.png" projectUrl="https://github.com/DodoSeal/LPlot" tags={[ "TypeScript", "Svelte", "Node.js", "Firebase", "Express.js" ]} />
+        <ProjectCard title="Islands Economy Discord Bot" description="Fetch Roblox Islands economy information, such as pricing guides and item rarities." imageSrc="/ProjectImages/IslandsBot.png" projectUrl="https://islandsbot.dodoseal.com" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
 
         <!-- Template Card -->
         <!-- <ProjectCard title="" description="" imageSrc="" projectUrl="" tags={[  ]} /> -->
+    </ProjectSection>
+
+    <ProjectSection sectionTitle="Airship">
+        <ProjectCard title="Airship Docs Discord Bot" description="Lookup resources from the Airship documentation." imageSrc="/ProjectImages/AirshipDocsBot.png" projectUrl="" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
+        <ProjectCard title="Walk to Win" description="A simple game where walking earns points. Made with Airship.gg" imageSrc="/ProjectImages/WalkToWin.png" projectUrl="https://github.com/DodoSeal/airship-walking" tags={[ "TypeScript", "Unity Engine", "Airship" ]} />
+        <ProjectCard title="Airship API Discord Bot" description="A powerful Discord Bot to fetch public information from Airship.gg." imageSrc="/ProjectImages/Airship.png" projectUrl="https://github.com/DodoSeal/AirshipAPI-DiscordBot" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
+        <ProjectCard title="Easy Games Notifier" description="A Discord bot that polls the Airship Games API to discover new content from Easy.gg." imageSrc="/ProjectImages/EasyNotifier.png" projectUrl="https://github.com/DodoSeal/EZGG-Notification-Bot" tags={[ "TypeScript", "Node.js", "Discord.js" ]} />
+        <ProjectCard title="Airship Bundle Downloader" description="Download Airship.gg Game Bundles using a CLI." imageSrc="/ProjectImages/AirshipBundleDownloader.png" projectUrl="https://github.com/DodoSeal/Airship-Bundle-Downloader" tags={[ "TypeScript", "Node.js" ]} />
+        <ProjectCard title="Airship CLI" description="Interact with the Airship.gg API from your Terminal!" imageSrc="/ProjectImages/AirshipCLI.png" projectUrl="https://github.com/DodoSeal/airship-cli" tags={[ "TypeScript", "Node.js" ]} />
+    </ProjectSection>
+
+    <div class="gap-6 grid grid-cols-1 md:grid-cols-2 pt-6">
     </div>
     <Footer />
 </div>
